6ad20d2d4bf8a17933a3739b5077fceb991aedfc
RajabovIlyas <ilyasrajabow@gmail.com>
2024-12-22 04:38:10 +0500

    feat: inited modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { TokensModule } from './tokens/tokens.module';
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, TokensModule, UsersModule, FilesModule],
 })
 export class AppModule {}
